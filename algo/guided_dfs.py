b56e6c4e3474651313eaf27b60cf26b427a81499
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2025-05-07 18:48:53 +0800

    Add move ordering sketch
    Tests have been crafted to ensure correctness of generated moves.
    
    Have not checked whether move ordering actually works.

---
+++ algo/guided_dfs.py
@@ -0,0 +1,99 @@
+from typing import Iterator
+from collections import defaultdict
+
+from state import State
+from .sorting import UnsolvablePuzzleException
+
+class GuidedAgent:
+    def __init__(self):
+        self.visited_states: set[State] = set()
+
+    def solve(self, puzzle: State) -> list[tuple[int, int]]:
+        self.visited_states.clear()
+        moves = self._solve(puzzle)
+        if moves is None:
+            raise UnsolvablePuzzleException()
+        return list(reversed(moves))
+
+    def _solve(self, puzzle: State) -> list[tuple[int, int]] | None:
+        if puzzle.is_terminal():
+            return []
+        
+        for actions, next_state in self._actions(puzzle):
+            result = self._solve(next_state)
+            if result is not None:
+                result.extend(list(reversed(actions)))
+                return result
+        
+        return None
+    
+    def _actions(self, state: State) -> Iterator[tuple[list[tuple[int, int]], State]]:
+        """Returns the list of actionables and their next states.
+        Each element is a tuple, where,
+        1. The first element is a list of actions that, when applied on current state,
+        returns the next state.
+        2. The second element is the next state.
+        """
+        if state.from_tube is not None:
+            return self._transfer_actions(state)
+        for i, tube in enumerate(state.balls):
+            if len(tube) == 0:
+                return self._fill_actions(state, i)
+        return self._transfer_actions(state)
+    
+    def _transfer_actions(self, state: State) -> Iterator[tuple[list[tuple[int, int]], State]]:
+        actions = state.actions()
+        self._sort_transfer_actions(state, actions)
+        for action in actions:
+            next_state = state.move(action)
+            if next_state in self.visited_states:
+                continue
+            self.visited_states.add(next_state)
+            yield [action], next_state
+    
+    def _sort_transfer_actions(self, state: State, actions: list[tuple[int, int]]):
+        tube_scores = self._get_tube_scores(state)
+        actions.sort(key=lambda action: -tube_scores[action[0]])
+    
+    def _get_tube_scores(self, state: State) -> list[int]:
+        colour_scores: defaultdict[int, int] = defaultdict(int)
+        for tube in state.balls:
+            if len(tube) == state.max_length or len(tube) == 0:
+                continue
+            if all(ball == tube[0] for ball in tube):
+                colour_scores[tube[0]] = 2
+            else:
+                colour_scores[tube[-1]] = max(colour_scores[tube[-1]], 1)
+
+        tube_scores: list[int] = []
+        for tube in state.balls:
+            score = 0
+            for i in range(len(tube) - 2, -1, -1):
+                if colour_scores[tube[i]] == 0:
+                    break
+                score += colour_scores[tube[i]]
+            tube_scores.append(score)
+        return tube_scores
+    
+    def _fill_actions(self, state: State, index: int) -> Iterator[tuple[list[tuple[int, int]], State]]:
+        colour_map: defaultdict[int, list[int]] = defaultdict(list)
+        for i, tube in enumerate(state.balls):
+            if len(tube) == 0:
+                continue
+            colour_map[tube[-1]].append(i)
+        
+        l: list[tuple[list[tuple[int, int]], State, int]] = []
+        for _, tubes in colour_map.items():
+            actions = [(tube, index) for tube in tubes]
+            next_state = state
+            for action in actions:
+                next_state = next_state.move(action)
+            if next_state in self.visited_states:
+                continue
+            self.visited_states.add(next_state)
+            tube_scores = self._get_tube_scores(next_state)
+            score = sum(tube_scores)
+            l.append((actions, next_state, score))
+        l.sort(key=lambda item: -item[2])
+        for actions, next_state, _ in l:
+            yield actions, next_state
---
+++ algo/guided_dfs_test.py
@@ -0,0 +1,180 @@
+import unittest
+
+from .guided_dfs import GuidedAgent
+from state import State
+
+one_move = State(
+    [
+        [1, 1, 1],
+        [2, 2, 2, 2],
+        [1],
+    ],
+    4,
+)
+
+four_moves = State(
+    [
+        [1, 1, 2, 1],
+        [2, 2, 2],
+        [1],
+    ],
+    4,
+)
+
+unsolvable = State(
+    [
+        [1, 1, 2],
+        [2, 2, 2],
+        [1],
+    ],
+    3,
+)
+
+split_move = State(
+    [
+        [0, 0, 1, 1],
+        [2, 2, 1],
+        [2, 2, 1],
+        [0, 0],
+    ],
+    4,
+)
+
+waiting_move = State(
+    [
+        [0, 0, 1, 1],
+        [0, 0, 1],
+        [1, 2, 2],
+        [2, 2],
+    ],
+    4,
+)
+
+timing_1 = State(
+    [
+        [3, 2, 1, 0],
+        [6, 4, 5, 4],
+        [7, 8, 7, 3],
+        [10, 9, 2, 8],
+        [9, 11, 10, 7],
+        [8, 1, 7, 0],
+        [9, 4, 1, 0],
+        [8, 6, 5, 2],
+        [11, 9, 11, 11],
+        [2, 3, 3, 4],
+        [6, 5, 10, 5],
+        [1, 0, 6, 10],
+        [],
+        [],
+    ],
+    4,
+)
+
+timing_2 = State(
+    [
+        [3, 2, 1, 0],
+        [2, 5, 4, 4],
+        [7, 6, 3, 0],
+        [10, 0, 9, 8],
+        [5, 8, 2, 5],
+        [10, 7, 4, 1],
+        [5, 0, 9, 6],
+        [10, 9, 9, 2],
+        [6, 1, 8, 11],
+        [3, 1, 8, 11],
+        [10, 11, 7, 11],
+        [4, 6, 3, 7],
+        [],
+        [],
+    ],
+    4,
+)
+
+timing_3 = State(
+    [
+        [0, 2, 1, 0],
+        [6, 5, 4, 3],
+        [7, 2, 4, 1],
+        [3, 0, 7, 8],
+        [10, 8, 9, 2],
+        [8, 2, 11, 9],
+        [5, 11, 11, 8],
+        [4, 10, 6, 3],
+        [9, 10, 11, 4],
+        [5, 0, 1, 6],
+        [5, 1, 6, 10],
+        [3, 7, 7, 9],
+        [],
+        [],
+    ],
+    4,
+)
+
+timing_unsolvable = State(
+    [
+        [2, 11, 4, 1],
+        [2, 11, 4, 11],
+        [9, 8, 4, 9],
+        [1, 7, 10, 9],
+        [10, 3, 12, 3],
+        [8, 12, 10, 7],
+        [7, 6, 10, 8],
+        [5, 2, 12, 5],
+        [9, 4, 6, 7],
+        [3, 3, 11, 12],
+        [8, 1, 1, 5],
+        [5, 6, 6, 2],
+        [],
+        [],
+    ],
+    4,
+)
+
+class TestMoves(unittest.TestCase):
+    def assert_correct_moves(self,
+                             generated_moves: list[tuple[int, int]],
+                             correct_moves: list[tuple[int, int]],
+                             ):
+        self.assertTrue(len(set(generated_moves)) == len(generated_moves),
+                        f"Duplicate moves found in {generated_moves}")
+        correct_moves = set(correct_moves)
+        for move in generated_moves:
+            self.assertTrue(move in correct_moves,
+                            f"Generated move {move} not found in {correct_moves}")
+    
+    def try_state(self, state: State):
+        agent = GuidedAgent()
+        correct_moves = state.actions()
+        for moves, _ in agent._actions(state):
+            self.assert_correct_moves(moves, correct_moves)
+    
+    def test_1_move(self):
+        self.try_state(one_move)
+    
+    def test_4_move(self):
+        self.try_state(four_moves)
+    
+    def test_unsolvable(self):
+        self.try_state(unsolvable)
+    
+    def test_split_move(self):
+        self.try_state(split_move)
+    
+    def test_waiting_move(self):
+        self.try_state(waiting_move)
+    
+    def test_timing_1(self):
+        self.try_state(timing_1)
+    
+    def test_timing_2(self):
+        self.try_state(timing_2)
+    
+    def test_timing_3(self):
+        self.try_state(timing_3)
+    
+    def test_timing_unsolvable(self):
+        self.try_state(timing_unsolvable)
+
+
+if __name__ == '__main__':
+    unittest.main()
